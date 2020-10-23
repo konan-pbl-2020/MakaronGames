eb00a0d2b206e2b879af88119958c32ce4b0a7ce
koheikosuga <s1971042@s.konan-u.ac.jp>
2020-10-23 16:22:26 +0900

    コミットしました

--- app/src/main/java/com/example/makarongames/SiritoriStart.java
+++ app/src/main/java/com/example/makarongames/SiritoriStart.java
@@ -2,7 +2,10 @@ package com.example.makarongames;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class SiritoriStart extends AppCompatActivity {
 
@@ -10,5 +13,14 @@ public class SiritoriStart extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_siritori_start);
+
+        Button PlayButton = findViewById(R.id.playbutton);
+        PlayButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplication(),SiritoriPlay.class);//SiritoriPlayへの画面遷移
+                startActivity(intent);
+            }
+        });
     }
 }
