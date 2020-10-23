9cb59557192f388d94bcbc12b5e8cae3dcdf89ac
TomohiroTK <tomopylori2000@ezweb.ne.jp>
2020-10-23 15:13:32 +0900

    とりあえず画面遷移図の通りに全画面作りました。

---
+++ app/src/main/java/com/example/makarongames/QuizHowTo.java
@@ -0,0 +1,14 @@
+package com.example.makarongames;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class QuizHowTo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_quiz_how_to);
+    }
+}
