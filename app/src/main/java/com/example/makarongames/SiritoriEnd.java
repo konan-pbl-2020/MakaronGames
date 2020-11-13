c0a57babc20c19f143e2cd7924c45a55167310dd
koheikosuga <s1971042@s.konan-u.ac.jp>
2020-11-13 17:38:27 +0900

    コミットしました

--- app/src/main/java/com/example/makarongames/SiritoriEnd.java
+++ app/src/main/java/com/example/makarongames/SiritoriEnd.java
@@ -7,6 +7,8 @@ import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import twitter4j.Twitter;
 import twitter4j.TwitterException;
@@ -18,6 +20,11 @@ public class SiritoriEnd extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_siritori_end);
+        TextView resultLabel = findViewById(R.id.resultLabel);
+
+        final int score = getIntent().getIntExtra("SCORE", 0);
+
+        resultLabel.setText(String.valueOf(score));
 
         Button nextButton = (Button)findViewById(R.id.tweet);
         nextButton.setOnClickListener(new View.OnClickListener() {
@@ -25,7 +32,9 @@ public class SiritoriEnd extends AppCompatActivity {
                 AsyncTask<Void,Void,String> task = new AsyncTask<Void, Void, String>(){
                     @Override
                     protected String doInBackground(Void... params) {
-                        String latestStatus = "○○さんのしりとりスコア：〇点";
+                        final EditText editText = findViewById(R.id.edit_text);
+                        String playername = editText.getText().toString();
+                        String latestStatus = playername+"さんのクイズスコア："+score+"点";
                         Twitter twitter = TwitterFactory.getSingleton();
                         try {
                             twitter4j.Status status = twitter.updateStatus(latestStatus);
@@ -39,5 +48,9 @@ public class SiritoriEnd extends AppCompatActivity {
             }
         });
     }
+//    public void returnTop(View view) {
+//        Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+//        startActivity(intent);
+//    }
 
 }
