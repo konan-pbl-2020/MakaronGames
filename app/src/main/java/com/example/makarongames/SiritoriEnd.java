a74e9b0208a81e57cea7e759433438bcd9266510
koheikosuga <s1971042@s.konan-u.ac.jp>
2020-11-13 14:40:33 +0900

    コミットしました

--- app/src/main/java/com/example/makarongames/SiritoriEnd.java
+++ app/src/main/java/com/example/makarongames/SiritoriEnd.java
@@ -2,7 +2,15 @@ package com.example.makarongames;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import twitter4j.Twitter;
+import twitter4j.TwitterException;
+import twitter4j.TwitterFactory;
 
 public class SiritoriEnd extends AppCompatActivity {
 
@@ -10,5 +18,26 @@ public class SiritoriEnd extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_siritori_end);
+        //しりとり画面へ遷移
+        Button nextButton = (Button)findViewById(R.id.tweet);
+        nextButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                AsyncTask<Void,Void,String> task = new AsyncTask<Void, Void, String>(){
+                    @Override
+                    protected String doInBackground(Void... params) {
+                        String latestStatus = "○○さんのしりとりスコア：〇点";
+                        Twitter twitter = TwitterFactory.getSingleton();
+                        try {
+                            twitter4j.Status status = twitter.updateStatus(latestStatus);
+                        } catch (TwitterException e) {
+                            e.printStackTrace();
+                        }
+                        return null;
+                    }
+                };
+                task.execute();
+            }
+        });
     }
+
 }
--- app/src/main/java/com/example/makarongames/SiritoriHowTo.java
+++ app/src/main/java/com/example/makarongames/SiritoriHowTo.java
@@ -2,7 +2,15 @@ package com.example.makarongames;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import twitter4j.Twitter;
+import twitter4j.TwitterException;
+import twitter4j.TwitterFactory;
 
 public class SiritoriHowTo extends AppCompatActivity {
 
@@ -10,5 +18,14 @@ public class SiritoriHowTo extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_siritori_how_to);
+
+        Button PlayButton = findViewById(R.id.text);
+        PlayButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplication(),SiritoriEnd.class);//SiritoriEndへの画面遷移
+                startActivity(intent);
+            }
+        });
     }
 }
